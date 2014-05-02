a15af06f51664de11a2621f249535eb12f14148b
Naxanria <gerbrandnijk@gmail.com>
2014-05-02 20:37:11 +0200

    formatting

--- src/com/ipsum/graphics/Screen.java
+++ src/com/ipsum/graphics/Screen.java
@@ -8,34 +8,35 @@ import org.lwjgl.opengl.DisplayMode;
 public class Screen
 {
 
-    private int width;
-    private int height;
+	private int width;
+	private int height;
 
-    public Screen(int width, int height)
-    {
-        this.width = width;
-        this.height = height;
+	public Screen(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
 
-        /* Create Screen */
-        try
-        {
-					Display.setDisplayMode(new DisplayMode(width, height));
-          Display.create();
-          Display.setResizable(true);
-        }
-        catch (LWJGLException e)
-        {
-            e.printStackTrace();
-        }
-    }
-	
-    public boolean isCloseRequested()
-    {
-        return org.lwjgl.opengl.Display.isCloseRequested();
-    }
-    public void Update(){
-        org.lwjgl.opengl.Display.update();
-    }
+		try
+		{
+			Display.setDisplayMode(new DisplayMode(width, height));
+			Display.create();
+			Display.setResizable(true);
+		}
+		catch (LWJGLException e)
+		{
+				e.printStackTrace();
+		}
+	}
+
+	public boolean isCloseRequested()
+	{
+		return Display.isCloseRequested();
+	}
+
+	public void Update()
+	{
+  	Display.update();
+  }
 
 
 }
